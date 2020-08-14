77ac9d54ab7e2266b6d784ef66467dbc2b0241f4
Gorka Lendrino <gorka.lendrinovela@thalesaleniaspace.com>
2020-08-14 10:33:35 +0200

    Second

--- app/src/main/java/com/example/bobesponja/CallActivity.java
+++ app/src/main/java/com/example/bobesponja/CallActivity.java
@@ -1,21 +1,22 @@
 package com.example.bobesponja;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CallActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_call);
     }
 
-    /** Called when the user taps the Call button */
-    public void gotoIncomingCallActivity(View view) {
-        Intent intent = new Intent(this, IncomingCallActivity.class);
+    /** Called when the user taps the Hang button */
+    public void gotoMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/bobesponja/IncomingCallActivity.java
+++ app/src/main/java/com/example/bobesponja/IncomingCallActivity.java
@@ -2,7 +2,10 @@ package com.example.bobesponja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.media.MediaPlayer;
 
 public class IncomingCallActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class IncomingCallActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incoming_call);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.bobesponjatonos);
+        mp.start();
+    }
+
+    /** Called when the user taps the Answer button */
+    public void gotoCallActivity(View view) {
+        Intent intent = new Intent(this, CallActivity.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user taps the Hang button */
+    public void gotoMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
